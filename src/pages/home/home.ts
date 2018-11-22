23a4bdd4945b8166ff04e21bfabdf6b8444cf4de
tester <test@test.com>
2018-11-23 02:07:40 +0700

    test 360 photo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 // import * as AFRAME from 'aframe';
+// import { AEntity } from 'aframe';
+// import { ANode } from 'aframe';
+// import { AScene } from 'aframe';
+// import { AComponent } from 'aframe';
 
 
 @Component({
