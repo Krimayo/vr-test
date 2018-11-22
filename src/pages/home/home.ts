c938572d6492d291258e35c48a1dc439907ccb06
tester <test@test.com>
2018-11-23 01:53:44 +0700

    photo 360

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// import * as AFRAME from 'aframe';
+
 
 @Component({
   selector: 'page-home',
@@ -8,7 +10,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    
   }
 
 }
